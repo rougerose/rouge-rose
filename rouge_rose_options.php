1c69bf344dd72cd7762d48d2decaa89b56ebafa2
christophe le drean <christophe@rouge-rose.net>
2023-12-09 17:38:24 +0100

    build: supprimer debug

--- rouge_rose_options.php
+++ rouge_rose_options.php
@@ -1,14 +1,17 @@
 <?php
-if (!defined('_ECRIRE_INC_VERSION')) return;
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+  return;
+}
 
 // Débug
-error_reporting(E_ALL^E_NOTICE);
-ini_set ("display_errors", "On");
-define('SPIP_ERREUR_REPORT', E_ALL);
-define('_NO_CACHE', -1);
-define('_INTERDIRE_COMPACTE_HEAD_ECRIRE', true);
-define('_LOG_FILELINE',true);
-define('_LOG_FILTRE_GRAVITE',8);
+// error_reporting(E_ALL^E_NOTICE);
+// ini_set ("display_errors", "On");
+// define('SPIP_ERREUR_REPORT', E_ALL);
+// define('_NO_CACHE', -1);
+// define('_INTERDIRE_COMPACTE_HEAD_ECRIRE', true);
+// define('_LOG_FILELINE',true);
+// define('_LOG_FILTRE_GRAVITE',8);
 
 
 if (!isset($GLOBALS['z_blocs'])) {
