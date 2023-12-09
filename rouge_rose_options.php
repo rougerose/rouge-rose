7bf768dfc09a583204014236af2e3638b6d1ee2d
christophe le drean <christophe@rouge-rose.net>
2023-12-09 16:17:08 +0100

    fix: mise à jour options

--- rouge_rose_options.php
+++ rouge_rose_options.php
@@ -1,6 +1,16 @@
-<?php 
+<?php
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
-if (!isset($GLOBALS['z_blocs']))
-    $GLOBALS['z_blocs'] = array('content','aside','extra','head','head_js','header','footer','breadcrumb');
-?>
+// Débug
+error_reporting(E_ALL^E_NOTICE);
+ini_set ("display_errors", "On");
+define('SPIP_ERREUR_REPORT', E_ALL);
+define('_NO_CACHE', -1);
+define('_INTERDIRE_COMPACTE_HEAD_ECRIRE', true);
+define('_LOG_FILELINE',true);
+define('_LOG_FILTRE_GRAVITE',8);
+
+
+if (!isset($GLOBALS['z_blocs'])) {
+	$GLOBALS['z_blocs'] = ['content', 'aside', 'extra', 'head', 'head_js', 'header', 'footer', 'breadcrumb'];
+}
