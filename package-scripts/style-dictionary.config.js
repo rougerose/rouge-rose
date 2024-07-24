d21b6600fd259d4da6b4044fed3e69197531ec5c
christophe le drean <christophe@rouge-rose.net>
2024-07-24 17:08:46 +0200

    feat: design tokens

---
+++ package-scripts/style-dictionary.config.js
@@ -0,0 +1,31 @@
+import StyleDictionary from "style-dictionary-utils";
+
+const config = {
+	source: ["src/design-tokens/**/*.json"],
+	platforms: {
+		scss: {
+			buildPath: "src/",
+			transforms: [
+				"attribute/cti",
+				"name/cti/kebab",
+				"time/seconds",
+				"content/icon",
+				"size/rem",
+				"color/hsl",
+				// utiliser fontFamily/css. Permet d'entourer de guillemets simples
+				// les noms de fontes avec espace.
+				"fontFamily/css",
+			],
+			files: [
+				{
+					destination: "scss/abstract/_variables.scss",
+					format: "scss/variables",
+					options: { outputReferences: true },
+				},
+			],
+		},
+	},
+};
+
+const sd = StyleDictionary.extend(config);
+sd.buildAllPlatforms();
