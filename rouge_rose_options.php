42c7b23be31c55b19cbc61635c4fd0e3f817a1b4
christophe le drean <christophe@rouge-rose.net>
2024-07-26 12:13:49 +0200

    fix: z_blocs

--- rouge_rose_options.php
+++ rouge_rose_options.php
@@ -15,5 +15,5 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 
 if (!isset($GLOBALS['z_blocs'])) {
-	$GLOBALS['z_blocs'] = ['content', 'aside', 'extra', 'head', 'head_js', 'header', 'footer', 'breadcrumb'];
+	$GLOBALS['z_blocs'] = ['content', 'head', 'head_js', 'header', 'footer', 'breadcrumb'];
 }
