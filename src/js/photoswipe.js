c27010800786e264f4c759c9389f67be2cbc5584
christophe le drean <christophe@rouge-rose.net>
2024-07-30 11:13:59 +0200

    feat: photoswipe

---
+++ src/js/photoswipe.js
@@ -0,0 +1,19 @@
+import PhotoSwipeLightbox from "photoswipe/lightbox";
+import PhotoSwipe from "photoswipe";
+
+/**
+ * Le script est chargé pour tous les articles,
+ * mais photoswipe n'est pas forcément nécessaire.
+ */
+let $portfolio = document.querySelectorAll(".object_portfolio .album");
+
+if ($portfolio) {
+	const lightbox = new PhotoSwipeLightbox({
+		gallery: $portfolio,
+		children: "a.photoswipe",
+		showHideAnimationType: "fade",
+		pswpModule: PhotoSwipe,
+	});
+
+	lightbox.init();
+}
